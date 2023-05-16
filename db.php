013bf3e687051cca9e5fe92d864b1f3a4d62fb4c
Luisicho <luism18.1999@gmail.com>
2023-05-16 14:24:59 -0600

    formulario add

--- db.php
+++ db.php
@@ -1,4 +1,3 @@
 <?php 
-session_start();
 $conex = mysqli_connect("localhost","root","","repuve");
 ?>
--- index.php
+++ index.php
@@ -1,19 +1,30 @@
-<?php include("db.php")?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Escalafon</title>
-    <!-- Bootstrap -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-</head>
-<body>
-    
-    <!-- Script -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js" integrity="sha384-zYPOMqeu1DAVkHiLqWBUTcbYfZ8osu1Nd6Z89ify25QV9guujx43ITvfi12/QExE" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.min.js" integrity="sha384-Y4oOpwW3duJdCWv5ly8SCFYWqFDsfob/3GkgExXKV4idmbt98QcxXYs9UoXAB7BZ" crossorigin="anonymous"></script>
-</body>
-</html>
+<?php include("db.php") ?>
+
+<?php include("includes/header.php") ?>
+
+<div class="container p-4">
+
+    <div class="row">
+        <div class="col-md-8">
+            <div class="card card-body">
+                <form action="save_esc.php" method="POST">
+                    <div class="form-group mt-2 mb-2">
+                        <label class="label">No. Escalafon</label>
+                        <input type="text" name="noesc" class="form-control" placeholder="No. Escalafon" autofocus>
+                    </div>
+                    <div class="form-group mt-2 mb-2">
+                        <label class="label">No. Escalafon</label>
+                        <input type="text" name="noesc" class="form-control" placeholder="No. Escalafon" autofocus>
+                    </div>
+                    <input type="submit" class="btn btn-success btn-block" name = "save" value = "Guardar">
+                </form>
+            </div>
+        </div>
+        <div class="col-md-8">
+
+        </div>
+    </div>
+
+</div>
+
+<?php include("includes/footer.php") ?>
